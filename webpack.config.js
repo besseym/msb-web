cc72df50ea81d937a3ca21c5cb5fad30ab02a07d
Michael Bessey <besseym@gmail.com>
2018-05-03 22:49:35 -0400

    updated dev dependancies and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
-const webpack = require('webpack');
-
 module.exports = {
-  entry: "./src/ts/index",
+  entry: __dirname + "/src/ts/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
   output: {
     path: __dirname + "/dist",
     filename: "msb-web.js",
     library: "msb-web",
-    libraryTarget: "umd",
-    umdNamedDefine: true
+    libraryTarget: "umd"
   },
   resolve: {
     // Add '.ts' and '.tsx' as a resolvable extension.
     extensions: [".ts", ".tsx", ".js"]
-  },
-  module: {
-    loaders: [
-      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-      { test: /\.tsx?$/, loader: "ts-loader" }
-    ]
   }
-}
+};
